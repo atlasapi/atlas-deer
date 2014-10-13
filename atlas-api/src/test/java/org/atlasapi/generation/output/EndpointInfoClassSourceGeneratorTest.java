ca78d8f78d38e37521d4b2849645e1e91efa13ea
Oli Hall <oli@metabroadcast.com>
2014-10-13 19:00:31 +0100

    More tests

--- atlas-api/src/test/java/org/atlasapi/generation/output/EndpointInfoClassSourceGeneratorTest.java
+++ atlas-api/src/test/java/org/atlasapi/generation/output/EndpointInfoClassSourceGeneratorTest.java
@@ -7,6 +7,8 @@ import org.atlasapi.generation.model.EndpointTypeInfo;
 import org.junit.Test;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.common.collect.ImmutableList;
+
 
 public class EndpointInfoClassSourceGeneratorTest {
     
@@ -34,6 +36,23 @@ public class EndpointInfoClassSourceGeneratorTest {
         String typeBasedFields = generator.generateTypeBasedFields(typeInfo);
         
         assertEquals(expected(typeInfo), typeBasedFields);
+    }
+    
+    @Test
+    public void testFullClassCreation() {
+        EndpointTypeInfo typeInfo = EndpointTypeInfo.builder()
+                .withKey("key")
+                .withDescription("description")
+                .withClassName("className")
+                .withProducedType("ProducedType")
+                .withRootPath("rootPath")
+                .build();
+        
+        EndpointMethodInfo methodInfo = new EndpointMethodInfo("path", RequestMethod.GET);
+        
+        String classString = generator.processType(typeInfo, ImmutableList.of(methodInfo));
+        
+        assertEquals(expectedClassString(typeInfo, methodInfo), classString);
     }
 
     private String expected(EndpointTypeInfo typeInfo) {
@@ -70,4 +89,56 @@ public class EndpointInfoClassSourceGeneratorTest {
         );
     }
 
+    private String expectedClassString(EndpointTypeInfo typeInfo, EndpointMethodInfo methodInfo) {
+        String expectedClassPattern = new StringBuilder()
+                .append("package org.atlasapi.generation.generated.endpoints;\n")
+                .append("\n")
+                .append("import java.util.Set;\n")
+                .append("import com.google.common.collect.ImmutableSet;\n")
+                .append("import org.atlasapi.generation.model.EndpointClassInfo;\n")
+                .append("import org.atlasapi.generation.model.Operation;\n")
+                .append("import org.springframework.web.bind.annotation.RequestMethod;\n")
+                .append("\n")
+                .append("\n")
+                .append("public class %s implements EndpointClassInfo {\n")
+                .append("\n")
+                .append("    private static final Set<Operation> operations = ImmutableSet.<Operation>builder()\n")
+                .append("            .add(\n")
+                .append("                new Operation(RequestMethod.%s, %s)\n")
+                .append("            )\n")
+                .append("            .build();\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public Set<Operation> operations() {\n")
+                .append("        return operations;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public String name() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public String description() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public String rootPath() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("}\n")
+                .toString();
+        
+        return String.format(
+                expectedClassPattern, 
+                typeInfo.className(),
+                methodInfo.method(),
+                methodInfo.path(),
+                typeInfo.producedType().toLowerCase(),
+                typeInfo.description(),
+                typeInfo.rootPath()
+        );
+    }
 }
--- atlas-api/src/test/java/org/atlasapi/generation/output/ModelInfoClassSourceGeneratorTest.java
+++ atlas-api/src/test/java/org/atlasapi/generation/output/ModelInfoClassSourceGeneratorTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.atlasapi.generation.model.JsonType;
 import org.atlasapi.generation.model.ModelMethodInfo;
 import org.atlasapi.generation.model.ModelTypeInfo;
-import org.atlasapi.generation.output.ModelClassInfoSourceGenerator;
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
 
 
 public class ModelInfoClassSourceGeneratorTest {
@@ -41,6 +42,29 @@ public class ModelInfoClassSourceGeneratorTest {
         String typeBasedFields = generator.generateTypeBasedFields(typeInfo);
         
         assertEquals(expected(typeInfo), typeBasedFields);
+    }
+    
+    @Test
+    public void testFullClassCreation() {
+        ModelTypeInfo typeInfo = ModelTypeInfo.builder()
+                .withKey("key")
+                .withDescription("description")
+                .withOutputClassName("outputClass")
+                .withParsedClass("parsedClass")
+                .build();
+        
+        ModelMethodInfo methodInfo = ModelMethodInfo.builder()
+                .withName("name")
+                .withDescription("description")
+                .withIsModelType(true)
+                .withIsMultiple(false)
+                .withJsonType(JsonType.NUMBER)
+                .withType("type")
+                .build();
+        
+        String classString = generator.processType(typeInfo, ImmutableList.of(methodInfo));
+        
+        assertEquals(expectedClassString(typeInfo, methodInfo), classString);
     }
 
     private String expected(ModelTypeInfo typeInfo) {
@@ -89,6 +113,70 @@ public class ModelInfoClassSourceGeneratorTest {
                 methodInfo.isMultiple().toString(), 
                 methodInfo.isModelType().toString(), 
                 methodInfo.jsonType().name()
+        );
+    }
+
+    private String expectedClassString(ModelTypeInfo typeInfo, ModelMethodInfo methodInfo) {
+        String expectedClassPattern = new StringBuilder()
+                .append("package org.atlasapi.generation.generated.model;\n")
+                .append("\n")
+                .append("import java.util.Set;\n")
+                .append("import com.google.common.collect.ImmutableSet;\n")
+                .append("import org.atlasapi.generation.model.ModelClassInfo;\n")
+                .append("import org.atlasapi.generation.model.FieldInfo;\n")
+                .append("import org.atlasapi.generation.model.JsonType;\n")
+                .append("\n")
+                .append("\n")
+                .append("public class %s implements ModelClassInfo {\n")
+                .append("\n")
+                .append("    private static final Set<FieldInfo> fields = ImmutableSet.<FieldInfo>builder()\n")
+                .append("            .add(\n")
+                .append("                FieldInfo.builder()\n")
+                .append("                    .withName(\"%s\")\n")
+                .append("                    .withDescription(\"%s\")\n")
+                .append("                    .withType(\"%s\")\n")
+                .append("                    .withIsMultiple(%s)\n")
+                .append("                    .withIsModelType(%s)\n")
+                .append("                    .withJsonType(JsonType.%s)\n")
+                .append("                    .build()\n")
+                .append("            )\n")
+                .append("            .build();\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public Set<FieldInfo> fields() {\n")
+                .append("        return fields;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public String key() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public String description() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("    @Override\n")
+                .append("    public Class<?> describedType() {\n")
+                .append("        return %s;\n")
+                .append("    }\n")
+                .append("\n")
+                .append("}\n")
+                .toString();
+        
+        return String.format(
+                expectedClassPattern, 
+                typeInfo.className(),
+                methodInfo.name(), 
+                methodInfo.description(), 
+                methodInfo.type(),
+                methodInfo.isMultiple().toString(), 
+                methodInfo.isModelType().toString(), 
+                methodInfo.jsonType().name(),
+                typeInfo.key(),
+                typeInfo.description(),
+                typeInfo.parsedClass()
         );
     }
 
