41debd998874600e7e7bc3f9f8b5efe26f993af2
Tom McAdam <tom@metabroadcast.com>
2014-12-09 13:17:23 +0000

    Improve error handling of invalid OAuth token
    Previously we would respond with a 500 HTTP response
    when presented with invalid credentials. This is incorrect,
    since the server is not at fault, but the request. By
    returning no user when we receive a 401 from Google, we'll
    deny the request if it's a protected URI.

--- atlas-api/src/main/java/org/atlasapi/application/auth/google/GoogleAccessTokenChecker.java
+++ atlas-api/src/main/java/org/atlasapi/application/auth/google/GoogleAccessTokenChecker.java
@@ -8,9 +8,11 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.repackaged.com.google.common.base.Throwables;
 import com.google.api.services.oauth2.model.Userinfo;
 import com.metabroadcast.common.base.Maybe;
+import com.metabroadcast.common.http.HttpStatusCode;
 import com.metabroadcast.common.social.auth.credentials.AuthToken;
 import com.metabroadcast.common.social.auth.facebook.AccessTokenChecker;
 import com.metabroadcast.common.social.model.UserRef;
@@ -38,10 +40,20 @@ public class GoogleAccessTokenChecker implements AccessTokenChecker {
         try {
             Userinfo user = googleClient.getUserForToken(accessToken);
             return Maybe.just(userRefBuilder.from(String.valueOf(user.getId()), UserNamespace.GOOGLE));
+        } catch (GoogleJsonResponseException e) {
+            if (HttpStatusCode.UNAUTHORIZED.is(e.getStatusCode())) {
+                //TODO We should change this interface to allow returning a response as well
+                //     as a user. It would be nice to bubble up the fact that a 401 response
+                //     was received from an AccessTokenChecker. In the meantime, we'll log
+                //     this.
+                log.info("Received a 401 response from Google: {}", e.getDetails());
+                return Maybe.<UserRef>nothing();
+            }
+            throw Throwables.propagate(e);
         } catch (IOException e) {
-            Throwables.propagate(e);
-            return null;
+            throw Throwables.propagate(e);
         }
+
     }
 
 }
