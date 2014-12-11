61db56092a194460c35d6c483470cfcc7fb7dc94
Jamie Perkins <jamie@metabroadcast.com>
2014-12-11 18:18:08 +0000

    Dont make up updated times

--- atlas-processing/src/main/java/org/atlasapi/system/bootstrap/workers/ContentReadWriteWorker.java
+++ atlas-processing/src/main/java/org/atlasapi/system/bootstrap/workers/ContentReadWriteWorker.java
@@ -20,7 +20,6 @@ import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.messaging.ResourceUpdatedMessage;
 import org.atlasapi.persistence.lookup.entry.LookupEntry;
 import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,10 +41,6 @@ public class ContentReadWriteWorker implements Worker<ResourceUpdatedMessage> {
 
         @Override
         public ResourceRef apply(Content input) {
-            if (input.getThisOrChildLastUpdated() != null) {
-                return input.toRef();
-            }
-            input.setThisOrChildLastUpdated(DateTime.now());
             return input.toRef();
         }
     };
