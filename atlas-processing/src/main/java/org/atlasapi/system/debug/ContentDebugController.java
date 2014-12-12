8c71ad1ddae1f0d5d2f304f9b20d22aabcbe9283
Jamie Perkins <jamie@metabroadcast.com>
2014-12-12 16:39:49 +0000

    More useful debugging endpoints

--- atlas-processing/src/main/java/org/atlasapi/system/debug/ContentDebugController.java
+++ atlas-processing/src/main/java/org/atlasapi/system/debug/ContentDebugController.java
@@ -4,21 +4,41 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import com.metabroadcast.common.base.Maybe;
-import org.atlasapi.content.*;
+import org.atlasapi.content.Content;
+import org.atlasapi.content.ContentResolver;
+import org.atlasapi.content.ContentStore;
+import org.atlasapi.content.EquivalentContentStore;
 import org.atlasapi.entity.Id;
+import org.atlasapi.entity.ResourceRef;
 import org.atlasapi.entity.util.Resolved;
+import org.atlasapi.entity.util.WriteException;
+import org.atlasapi.entity.util.WriteResult;
+import org.atlasapi.equivalence.EquivalenceGraphStore;
+import org.atlasapi.equivalence.EquivalenceGraphUpdate;
+import org.atlasapi.media.entity.LookupRef;
 import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.lookup.entry.LookupEntry;
+import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.gson.Gson;
@@ -27,7 +47,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.metabroadcast.common.base.Maybe;
 
 @Controller
 public class ContentDebugController {
@@ -39,18 +59,42 @@ public class ContentDebugController {
         }
     }).create();
 
-    private final ContentResolver resolver;
-    private final EquivalentContentStore equivalentContentStore;
+    private static final Function<Content, ResourceRef> TO_RESOURCE_REF = new Function<Content, ResourceRef>() {
 
-    public ContentDebugController(ContentResolver resolver, EquivalentContentStore equivalentContentStore) {
-        this.resolver = checkNotNull(resolver);
+        @Override
+        public ResourceRef apply(Content input) {
+            return input.toRef();
+        }
+    };
+    private static final Function<ResourceRef, Publisher> TO_SOURCE = new Function<ResourceRef, Publisher>() {
+
+        @Override
+        public Publisher apply(ResourceRef input) {
+            return input.getPublisher();
+        }
+    };
+
+    private final Logger log = LoggerFactory.getLogger(ContentDebugController.class);
+    private final ContentResolver legacyResolver;
+    private final EquivalentContentStore equivalentContentStore;
+    private final EquivalenceGraphStore graphStore;
+    private final ContentStore contentStore;
+    private final LookupEntryStore entryStore;
+
+    public ContentDebugController(ContentResolver legacyResolver, EquivalentContentStore equivalentContentStore,
+                                  EquivalenceGraphStore graphStore, ContentStore contentStore,
+                                  LookupEntryStore entryStore) {
+        this.legacyResolver = checkNotNull(legacyResolver);
         this.equivalentContentStore = checkNotNull(equivalentContentStore);
+        this.graphStore = checkNotNull(graphStore);
+        this.contentStore = checkNotNull(contentStore);
+        this.entryStore = checkNotNull(entryStore);
     }
 
     @RequestMapping("/system/debug/content/{id}")
     public void printContent(@PathVariable("id") Long id, final HttpServletResponse response) {
         ImmutableList<Id> ids = ImmutableList.of(Id.valueOf(id));
-        Futures.addCallback(resolver.resolveIds(ids), new FutureCallback<Resolved<Content>>() {
+        Futures.addCallback(contentStore.resolveIds(ids), new FutureCallback<Resolved<Content>>() {
 
             @Override
             public void onSuccess(Resolved<Content> result) {
@@ -73,7 +117,7 @@ public class ContentDebugController {
         });
     }
 
-    @RequestMapping("/system/debug/content/{id}/update-equiv")
+    @RequestMapping("/system/debug/content/{id}/migrate")
     public void forceEquivUpdate(@PathVariable("id") Long id, @RequestParam(value = "publisher", required = true) String publisherKey,
                                  final HttpServletResponse response) throws IOException {
         try {
@@ -81,19 +125,55 @@ public class ContentDebugController {
             if (publisherMaybe.isNothing()) {
                 response.setStatus(400);
                 response.getWriter().write("Supply a valid publisher key");
-                response.flushBuffer();
                 return;
             }
 
-            Publisher publisher = publisherMaybe.requireValue();
-            ItemRef ref = new ItemRef(Id.valueOf(id), publisher, "sortKey", DateTime.now());
-            equivalentContentStore.updateContent(ref);
+            Resolved<Content> resolved = Futures.getUnchecked(legacyResolver.resolveIds(ImmutableList.of(Id.valueOf(id))));
+            WriteResult<Content, Content> writeResult = contentStore.writeContent(resolved.getResources().first().get());
+
+            if (!writeResult.written()) {
+                response.getWriter().write("No write occured when migrating content into C* store");
+                response.setStatus(500);
+                return;
+            }
+
+            Optional<EquivalenceGraphUpdate> graphUpdate = migrateExplicitEquivalence(writeResult.getResource());
+            equivalentContentStore.updateEquivalences(graphUpdate.get());
 
             response.setStatus(200);
-            response.getWriter().write("Updated content equivalence for " + id.toString());
+            response.getWriter().write("Migrated content " + resolved.getResources().first().get().getId().toString());
             response.flushBuffer();
         } catch (Throwable t) {
             t.printStackTrace(response.getWriter());
         }
+    }
+
+    private Optional<EquivalenceGraphUpdate> migrateExplicitEquivalence(Content content) {
+        long id = content.getId().longValue();
+        try {
+            LookupEntry entry = Iterables.getOnlyElement(entryStore.entriesForIds(ImmutableList.of(id)));
+            if (entry.explicitEquivalents() == null || entry.explicitEquivalents().isEmpty()) {
+                throw new IllegalArgumentException("Content " + id + " has no explicit equivalents");
+            } else {
+                return updateEquivalences(content, entry);
+            }
+        } catch (WriteException | ExecutionException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    private Optional<EquivalenceGraphUpdate> updateEquivalences(Content content, LookupEntry entry) throws WriteException, ExecutionException {
+        ImmutableSet<ResourceRef> refs = resolveEquivRefsToResourceRefs(entry.explicitEquivalents());
+        log.info("Resolved {}/{} equivalent refs for content {}",
+                refs.size(), entry.explicitEquivalents().size(), content.getId());
+        ImmutableSet<Publisher> sources = FluentIterable.from(refs).transform(TO_SOURCE).toSet();
+        Optional<EquivalenceGraphUpdate> update = graphStore.updateEquivalences(content.toRef(), refs, sources);
+        return update;
+    }
+
+    private ImmutableSet<ResourceRef> resolveEquivRefsToResourceRefs(Set<LookupRef> lookupRefs) throws ExecutionException {
+        ImmutableSet<Id> ids = FluentIterable.from(lookupRefs).transform(LookupRef.TO_ID).transform(Id.fromLongValue()).toSet();
+        Resolved<Content> contentResolved = Futures.get(legacyResolver.resolveIds(ids), ExecutionException.class);
+        return contentResolved.getResources().transform(TO_RESOURCE_REF).toSet();
     }
 }
--- atlas-processing/src/main/java/org/atlasapi/system/debug/DebugModule.java
+++ atlas-processing/src/main/java/org/atlasapi/system/debug/DebugModule.java
@@ -1,20 +1,27 @@
 package org.atlasapi.system.debug;
 
 import org.atlasapi.AtlasPersistenceModule;
+import org.atlasapi.system.legacy.LegacyPersistenceModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(AtlasPersistenceModule.class)
+@Import({AtlasPersistenceModule.class, LegacyPersistenceModule.class})
 public class DebugModule {
-    
-    @Autowired private AtlasPersistenceModule persistenceModule;
-    
+
+    @Autowired
+    private AtlasPersistenceModule persistenceModule;
+    @Autowired
+    private LegacyPersistenceModule legacyPersistenceModule;
+
     @Bean
     public ContentDebugController contentDebugController() {
-        return new ContentDebugController(persistenceModule.contentStore(), persistenceModule.getEquivalentContentStore());
+        return new ContentDebugController(
+                legacyPersistenceModule.legacyContentResolver(),persistenceModule.getEquivalentContentStore(),
+                persistenceModule.getContentEquivalenceGraphStore(), persistenceModule.contentStore(),
+                legacyPersistenceModule.legacyEquivalenceStore());
     }
-    
+
 }
