28eca496b84b8b4064dd5d40bd51d46459805bb3
Jamie Perkins <jamie@metabroadcast.com>
2014-12-15 15:27:24 +0000

    metrics probe wiring

--- atlas-api/src/main/java/org/atlasapi/system/HealthModule.java
+++ atlas-api/src/main/java/org/atlasapi/system/HealthModule.java
@@ -1,5 +1,13 @@
 package org.atlasapi.system;
 
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.ImmutableList;
 import com.metabroadcast.common.health.HealthProbe;
@@ -7,42 +15,37 @@ import com.metabroadcast.common.health.probes.DiskSpaceProbe;
 import com.metabroadcast.common.health.probes.MemoryInfoProbe;
 import com.metabroadcast.common.webapp.health.HealthController;
 import com.metabroadcast.common.webapp.health.probes.MetricsProbe;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import java.util.Collection;
 
 @Configuration
 public class HealthModule {
-        
-        private final ImmutableList<HealthProbe> systemProbes = ImmutableList.of(
-            new MemoryInfoProbe(), 
+
+    private final ImmutableList<HealthProbe> systemProbes = ImmutableList.of(
+            new MemoryInfoProbe(),
             new DiskSpaceProbe()
-        );
-        
-        private @Autowired Collection<HealthProbe> probes;
-        private @Autowired HealthController healthController;
+    );
 
-        public @Bean HealthController healthController() {
-                return new HealthController(systemProbes);
-        }
-        
-        public @Bean org.atlasapi.system.HealthController threadController() {
-                return new org.atlasapi.system.HealthController();
-        }
+    private @Autowired Collection<HealthProbe> probes;
+    private @Autowired HealthController healthController;
 
-        public @Bean HealthProbe metricsProbe() {
-            return new MetricsProbe(metrics());
-        }
+    public @Bean HealthController healthController() {
+        return new HealthController(systemProbes);
+    }
 
-        public @Bean MetricRegistry metrics() {
-            return new MetricRegistry();
-        }
-        
-        @PostConstruct
-        public void addProbes() {
-                healthController.addProbes(probes);
-        }
+    public @Bean org.atlasapi.system.HealthController threadController() {
+        return new org.atlasapi.system.HealthController();
+    }
+
+    public @Bean HealthProbe metricsProbe() {
+        return new MetricsProbe(metrics());
+    }
+
+    public @Bean MetricRegistry metrics() {
+        return new MetricRegistry();
+    }
+
+    @PostConstruct
+    public void addProbes() {
+        healthController.addProbes(ImmutableList.of(metricsProbe()));
+        healthController.addProbes(probes);
+    }
 }
