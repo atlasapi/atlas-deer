dac9ddd52c35e80e637adf4530d77115b5688d50
Wojtek Wajerowicz <wojtek@metabroadcast.com>
2015-03-31 19:05:16 +0100

    Add blackout restriction to protocol buffer serializers.

---
+++ atlas-cassandra/src/main/java/org/atlasapi/content/BlackoutRestrictionSerializer.java
@@ -0,0 +1,18 @@
+package org.atlasapi.content;
+
+import org.atlasapi.serialization.protobuf.ContentProtos;
+
+public class BlackoutRestrictionSerializer {
+
+    public ContentProtos.BlackoutRestriction.Builder serialize(BlackoutRestriction blackoutRestriction) {
+        ContentProtos.BlackoutRestriction.Builder builder = ContentProtos.BlackoutRestriction.newBuilder();
+        if(blackoutRestriction.getAll() != null) {
+            builder.setAll(blackoutRestriction.getAll());
+        }
+        return builder;
+    }
+
+    public BlackoutRestriction deserialize(ContentProtos.BlackoutRestriction msg) {
+        return msg.hasAll() ? new BlackoutRestriction(msg.getAll()) : new BlackoutRestriction(null);
+    }
+}
