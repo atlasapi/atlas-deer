87f69c8d5d0038d367ea53ba18d312098df4f6e4
Jamie Perkins <jamie@metabroadcast>
2015-05-22 11:15:03 +0100

    Test new fields

--- atlas-cassandra/src/test/java/org/atlasapi/content/LocationSerializerTest.java
+++ atlas-cassandra/src/test/java/org/atlasapi/content/LocationSerializerTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import java.util.Currency;
 
 import org.atlasapi.entity.Id;
+import org.atlasapi.entity.PlayerRef;
 import org.atlasapi.entity.ServiceRef;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.serialization.protobuf.ContentProtos;
@@ -41,6 +42,8 @@ public class LocationSerializerTest {
         policy.setDrmPlayableFrom(new DateTime(DateTimeZones.UTC));
         policy.setNetwork(Policy.Network.WIFI);
         policy.setPlatform(Policy.Platform.IOS);
+        policy.setServiceRef(new ServiceRef(Id.valueOf(10l), Publisher.METABROADCAST));
+        policy.setPlayerRef(new PlayerRef(Id.valueOf(20L), Publisher.METABROADCAST));
         policy.setPrice(new Price(Currency.getInstance("GBP"), 400));
         policy.setRevenueContract(Policy.RevenueContract.PAY_TO_BUY);
         
@@ -67,6 +70,8 @@ public class LocationSerializerTest {
         assertThat(deserializedPolicy.getDrmPlayableFrom(), is(policy.getDrmPlayableFrom()));
         assertThat(deserializedPolicy.getNetwork(), is(policy.getNetwork()));
         assertThat(deserializedPolicy.getPlatform(), is(policy.getPlatform()));
+        assertThat(deserializedPolicy.getServiceRef(), is(policy.getServiceRef()));
+        assertThat(deserializedPolicy.getPlayerRef(), is(policy.getPlayerRef()));
         assertThat(deserializedPolicy.getPrice(), is(policy.getPrice()));
         assertThat(deserializedPolicy.getRevenueContract(), is(policy.getRevenueContract()));
         
