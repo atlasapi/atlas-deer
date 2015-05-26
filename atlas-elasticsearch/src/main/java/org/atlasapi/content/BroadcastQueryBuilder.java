f7f2f6c25e8eba7daa912b820f1a515e6de3ec4f
Jamie Perkins <jamie@metabroadcast>
2015-05-26 11:48:45 +0100

    Provide script language

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
@@ -13,25 +13,15 @@ import org.joda.time.DateTime;
 
 public class BroadcastQueryBuilder {
 
-    public static final String SCRIPT = ""
-            + "if (_source.broadcasts != null) {"
-            + "  now = time();"
-            + "  t = Long.MAX_VALUE;"
-            + "  f = 1;"
-            + "  foreach (b : _source.broadcasts) {"
-            + "    candidate = abs(now - b.transmissionTimeInMillis);"
-            + "    if (candidate < t) t = candidate;"
-            + "    if (b.repeat = false) f = firstBroadcastBoost;"
-            + "  }"
-            + "  _score + (_score * f * timeBoost * (1 / (1 + (t / (t < oneWeek ? 50 : 1)))));"
-            + "} else _score;";
+    public static final String SCRIPT = "score_script";
+    public static final String SCRIPT_LANG = "groovy";
 
     public static QueryBuilder build(QueryBuilder childQuery, Float timeBoost, Float firstBroadcastBoost) {
         return QueryBuilders.functionScoreQuery(
                 QueryBuilders.filteredQuery(childQuery,
                         FilterBuilders.nestedFilter(EsContent.BROADCASTS,
                                 FilterBuilders.rangeFilter(EsBroadcast.TRANSMISSION_TIME).from(new DateTime().minusDays(30)).to(new DateTime().plusDays(30)))))
-                .add(ScoreFunctionBuilders.scriptFunction(SCRIPT, ImmutableMap.of(
+                .add(ScoreFunctionBuilders.scriptFunction(SCRIPT, SCRIPT_LANG, ImmutableMap.of(
                         "firstBroadcastBoost", firstBroadcastBoost,
                         "timeBoost", timeBoost,
                         "oneWeek", TimeUnit.MILLISECONDS.convert(7, TimeUnit.DAYS)
