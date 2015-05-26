9d7c775a72d5aee54bb3d4c3a254f35e66b9af10
Jamie Perkins <jamie@metabroadcast>
2015-05-26 12:58:18 +0100

    Revert back to providing Groovy script to ES per request

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
@@ -2,25 +2,38 @@ package org.atlasapi.content;
 
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.collect.ImmutableMap;
-import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilder;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.joda.time.DateTime;
 
+import com.google.common.collect.ImmutableMap;
+
 public class BroadcastQueryBuilder {
 
-    public static final String SCRIPT = "score_script";
+    public static final String SCRIPT = ""
+            + "if (_source.broadcasts != null) {"
+            + "  now = time();"
+            + "  t = Long.MAX_VALUE;"
+            + "  f = 1;"
+            + "  foreach (b : _source.broadcasts) {"
+            + "    candidate = abs(now - b.transmissionTimeInMillis);"
+            + "    if (candidate < t) t = candidate;"
+            + "    if (b.repeat = false) f = firstBroadcastBoost;"
+            + "  }"
+            + "  _score + (_score * f * timeBoost * (1 / (1 + (t / (t < oneWeek ? 50 : 1)))));"
+            + "} else _score;";
     public static final String SCRIPT_LANG = "groovy";
 
-    public static QueryBuilder build(QueryBuilder childQuery, Float timeBoost, Float firstBroadcastBoost) {
+    public static QueryBuilder build(QueryBuilder childQuery, Float timeBoost,
+            Float firstBroadcastBoost) {
         return QueryBuilders.functionScoreQuery(
                 QueryBuilders.filteredQuery(childQuery,
                         FilterBuilders.nestedFilter(EsContent.BROADCASTS,
-                                FilterBuilders.rangeFilter(EsBroadcast.TRANSMISSION_TIME).from(new DateTime().minusDays(30)).to(new DateTime().plusDays(30)))))
+                                FilterBuilders.rangeFilter(EsBroadcast.TRANSMISSION_TIME)
+                                        .from(new DateTime().minusDays(30))
+                                        .to(new DateTime().plusDays(30)))))
                 .add(ScoreFunctionBuilders.scriptFunction(SCRIPT, SCRIPT_LANG, ImmutableMap.of(
                         "firstBroadcastBoost", firstBroadcastBoost,
                         "timeBoost", timeBoost,
