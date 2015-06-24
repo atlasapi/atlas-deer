87fe29dbaaa1b5cc27f797dcc1480466ec161115
Wojtek Wajerowicz <wojtek@metabroadcast.com>
2015-06-24 11:46:35 +0100

    Sort item summaries.

--- atlas-core/src/main/java/org/atlasapi/content/ItemSummary.java
+++ atlas-core/src/main/java/org/atlasapi/content/ItemSummary.java
@@ -1,6 +1,7 @@
 package org.atlasapi.content;
 
 import com.google.common.base.Objects;
+import com.google.common.collect.Ordering;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
@@ -8,6 +9,24 @@ import java.util.Optional;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ItemSummary {
+
+    public static final Ordering<ItemSummary> ORDERING = new Ordering<ItemSummary>() {
+        @Override
+        public int compare(@Nullable ItemSummary left, @Nullable ItemSummary right) {
+            if (left instanceof EpisodeSummary && right instanceof EpisodeSummary) {
+                EpisodeSummary leftES = (EpisodeSummary) left;
+                EpisodeSummary rightES = (EpisodeSummary) right;
+                return leftES.getEpisodeNumber().orElse(Integer.MAX_VALUE).compareTo(
+                        rightES.getEpisodeNumber().orElse(Integer.MAX_VALUE)
+                );
+            } else if (left instanceof EpisodeSummary && ((EpisodeSummary) left).getEpisodeNumber().isPresent()) {
+                return -1;
+            } else if (right instanceof EpisodeSummary && ((EpisodeSummary) right).getEpisodeNumber().isPresent()) {
+                return 1;
+            }
+            return 0;
+        }
+    };
 
     private final ItemRef itemRef;
     private final String title;
---
+++ atlas-core/src/test/java/org/atlasapi/content/ItemSummaryTest.java
@@ -0,0 +1,71 @@
+package org.atlasapi.content;
+
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
+import org.atlasapi.entity.Id;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.util.ImmutableCollectors;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+public class ItemSummaryTest {
+    @Test
+    public void testItemSummaryOrdering() {
+        ItemSummary itemSummary1 = new ItemSummary(
+                new ItemRef(Id.valueOf(1), Publisher.METABROADCAST, "", DateTime.now()),
+                "title1",
+                "desc",
+                "im"
+        );
+        EpisodeSummary itemSummary2 = new EpisodeSummary(
+                new ItemRef(Id.valueOf(2), Publisher.METABROADCAST, "", DateTime.now()),
+                "title1",
+                "desc",
+                "im",
+                null
+        );
+        EpisodeSummary itemSummary3 = new EpisodeSummary(
+                new ItemRef(Id.valueOf(3), Publisher.METABROADCAST, "", DateTime.now()),
+                "title1",
+                "desc",
+                "im",
+                1
+        );
+        EpisodeSummary itemSummary4 = new EpisodeSummary(
+                new ItemRef(Id.valueOf(4), Publisher.METABROADCAST, "", DateTime.now()),
+                "title1",
+                "desc",
+                "im",
+                2
+        );
+        EpisodeSummary itemSummary5 = new EpisodeSummary(
+                new ItemRef(Id.valueOf(5), Publisher.METABROADCAST, "", DateTime.now()),
+                "title1",
+                "desc",
+                "im",
+                3
+        );
+
+        List<ItemSummary> itemSummaries = ImmutableList.of(itemSummary1, itemSummary2, itemSummary3, itemSummary4, itemSummary5);
+
+
+        for (List<ItemSummary> summaries : Collections2.permutations(itemSummaries)) {
+            ImmutableList<ItemSummary> sortedSummaries = summaries
+                    .stream()
+                    .sorted(ItemSummary.ORDERING)
+                    .collect(ImmutableCollectors.toList());
+            assertThat(sortedSummaries.get(0), sameInstance(itemSummary3));
+            assertThat(sortedSummaries.get(1), sameInstance(itemSummary4));
+            assertThat(sortedSummaries.get(2), sameInstance(itemSummary5));
+            assertThat(sortedSummaries.subList(3,5), containsInAnyOrder(itemSummary1, itemSummary2));
+        }
+
+
+    }
+}
