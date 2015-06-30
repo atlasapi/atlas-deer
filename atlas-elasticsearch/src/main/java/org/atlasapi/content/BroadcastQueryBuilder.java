72d08728fa4cfd27238b32fd5511b5ecaedf0fda
Jamie Perkins <jamie@metabroadcast>
2015-06-30 17:47:30 +0100

    Correct timestamps

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
@@ -1,17 +1,20 @@
 package org.atlasapi.content;
 
+import com.metabroadcast.common.time.DateTimeZones;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.joda.time.DateTime;
 
+import java.util.Date;
+
 public class BroadcastQueryBuilder {
 
 
     public static QueryBuilder build(QueryBuilder childQuery, Float timeBoost) {
-        DateTime minusThirtyDays = DateTime.now().minusDays(30);
-        DateTime plusThirtyDays = DateTime.now().plusDays(30);
+        Date minusThirtyDays = DateTime.now().minusDays(30).toDateTime(DateTimeZones.UTC).toDate();
+        Date plusThirtyDays = DateTime.now().plusDays(30).toDateTime(DateTimeZones.UTC).toDate();
 
         return QueryBuilders.functionScoreQuery(
                 QueryBuilders.filteredQuery(
@@ -25,8 +28,8 @@ public class BroadcastQueryBuilder {
                 )
         ).add(ScoreFunctionBuilders.gaussDecayFunction(
                         EsBroadcast.TRANSMISSION_TIME_IN_MILLIS,
-                        DateTime.now().getMillis(),
-                        minusThirtyDays.getMillis()
+                        new Date().toInstant().getEpochSecond() * 1000,
+                        minusThirtyDays.toInstant().getEpochSecond() * 1000
                 )
         ).boost(timeBoost);
     }
