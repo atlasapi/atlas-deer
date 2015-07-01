b976f989edf2b08802dbf4210f2255dfed426aa8
Jamie Perkins <jamie@metabroadcast>
2015-07-01 14:44:15 +0100

    Broadcast scoring

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/BroadcastQueryBuilder.java
@@ -5,6 +5,7 @@ import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.search.MultiValueMode;
 import org.joda.time.DateTime;
 
 import java.util.Date;
@@ -14,23 +15,16 @@ public class BroadcastQueryBuilder {
 
     public static QueryBuilder build(QueryBuilder childQuery, Float timeBoost) {
         Date minusThirtyDays = DateTime.now().minusDays(30).toDateTime(DateTimeZones.UTC).toDate();
-        Date plusThirtyDays = DateTime.now().plusDays(30).toDateTime(DateTimeZones.UTC).toDate();
+        long minusThirtyDaysMillis = minusThirtyDays.toInstant().getEpochSecond() * 1000;
+        long nowMilis = new Date().toInstant().getEpochSecond() * 1000;
 
         return QueryBuilders.functionScoreQuery(
-                QueryBuilders.filteredQuery(
-                        childQuery,
-                        FilterBuilders.nestedFilter(
-                                EsContent.BROADCASTS,
-                                FilterBuilders.rangeFilter(EsBroadcast.TRANSMISSION_TIME)
-                                        .from(plusThirtyDays)
-                                        .to(minusThirtyDays)
-                        )
-                )
+                childQuery
         ).add(ScoreFunctionBuilders.gaussDecayFunction(
                         EsBroadcast.TRANSMISSION_TIME_IN_MILLIS,
-                        new Date().toInstant().getEpochSecond() * 1000,
-                        minusThirtyDays.toInstant().getEpochSecond() * 1000
-                )
+                        nowMilis,
+                        minusThirtyDaysMillis
+                ).setMultiValueMode(MultiValueMode.MIN)
         ).boost(timeBoost);
     }
 }
