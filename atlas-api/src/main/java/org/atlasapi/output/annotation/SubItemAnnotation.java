6318530bcaf09b77e91c414be4f0e75ab49f98b0
Jamie Perkins <jamie@metabroadcast>
2015-08-03 18:08:02 +0100

    Paginate over subitems through query params

--- atlas-api/src/main/java/org/atlasapi/output/annotation/SubItemAnnotation.java
+++ atlas-api/src/main/java/org/atlasapi/output/annotation/SubItemAnnotation.java
@@ -1,6 +1,7 @@
 package org.atlasapi.output.annotation;
 
 
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
 import com.metabroadcast.common.ids.NumberToShortStringCodec;
@@ -10,12 +11,15 @@ import org.atlasapi.content.ItemRef;
 import org.atlasapi.output.FieldWriter;
 import org.atlasapi.output.OutputContext;
 import org.atlasapi.output.writers.ItemRefWriter;
+import org.atlasapi.util.ImmutableCollectors;
 
 import java.io.IOException;
 
 
 public class SubItemAnnotation extends OutputAnnotation<Content> {
 
+    private static final String SUB_ITEM_OFFSET = "sub_items.offset";
+    private static final String SUB_ITEM_LIMIT = "sub_items.limit";
     private final ItemRefWriter childRefWriter;
 
     public SubItemAnnotation(NumberToShortStringCodec idCodec) {
@@ -25,12 +29,28 @@ public class SubItemAnnotation extends OutputAnnotation<Content> {
     @Override
     public void write(Content entity, FieldWriter writer, OutputContext ctxt) throws IOException {
         if (entity instanceof Container) {
+
+            Integer offset = 0;
+            Integer limit = Integer.MAX_VALUE;
+            String offsetStr = ctxt.getRequest().getParameter(SUB_ITEM_OFFSET);
+            if (!Strings.isNullOrEmpty(offsetStr)) {
+                offset = Integer.valueOf(offsetStr);
+            }
+            String limitStr = ctxt.getRequest().getParameter(SUB_ITEM_LIMIT);
+            if (!Strings.isNullOrEmpty(limitStr)) {
+                limit = Integer.valueOf(limitStr);
+            }
+
             Container container = (Container) entity;
             ImmutableList<ItemRef> orderedRefs = Ordering.natural()
                     .onResultOf(ItemRef::getSortKey)
                     .reverse()
                     .nullsLast()
                     .immutableSortedCopy(container.getItemRefs());
+            orderedRefs = orderedRefs.stream()
+                    .skip(offset)
+                    .limit(limit)
+                    .collect(ImmutableCollectors.toList());
             writer.writeList(childRefWriter, orderedRefs, ctxt);
         }
     }
--- atlas-api/src/main/java/org/atlasapi/output/annotation/SubItemSummariesAnnotations.java
+++ atlas-api/src/main/java/org/atlasapi/output/annotation/SubItemSummariesAnnotations.java
@@ -1,5 +1,6 @@
 package org.atlasapi.output.annotation;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
 import org.atlasapi.content.Container;
@@ -8,6 +9,7 @@ import org.atlasapi.content.ItemSummary;
 import org.atlasapi.output.FieldWriter;
 import org.atlasapi.output.OutputContext;
 import org.atlasapi.output.writers.SubItemSummaryListWriter;
+import org.atlasapi.util.ImmutableCollectors;
 
 import java.io.IOException;
 
@@ -15,6 +17,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class SubItemSummariesAnnotations extends OutputAnnotation<Content> {
 
+    private static final String SUB_ITEM_OFFSET = "sub_items.offset";
+    private static final String SUB_ITEM_LIMIT = "sub_items.limit";
     private final SubItemSummaryListWriter subItemSummaryListWriter;
 
     public SubItemSummariesAnnotations(SubItemSummaryListWriter subItemSummaryListWriter) {
@@ -24,12 +28,27 @@ public class SubItemSummariesAnnotations extends OutputAnnotation<Content> {
     @Override
     public void write(Content entity, FieldWriter writer, OutputContext ctxt) throws IOException {
         if(entity instanceof Container) {
+            Integer offset = 0;
+            Integer limit = Integer.MAX_VALUE;
+            String offsetStr = ctxt.getRequest().getParameter(SUB_ITEM_OFFSET);
+            if (!Strings.isNullOrEmpty(offsetStr)) {
+                offset = Integer.valueOf(offsetStr);
+            }
+            String limitStr = ctxt.getRequest().getParameter(SUB_ITEM_LIMIT);
+            if (!Strings.isNullOrEmpty(limitStr)) {
+                limit = Integer.valueOf(limitStr);
+            }
+
             Container container = (Container) entity;
             ImmutableList<ItemSummary> summaries = Ordering.natural()
                     .onResultOf((ItemSummary summary) -> summary.getItemRef().getSortKey())
                     .reverse()
                     .nullsLast()
                     .immutableSortedCopy(container.getItemSummaries());
+            summaries = summaries.stream()
+                    .skip(offset)
+                    .limit(limit)
+                    .collect(ImmutableCollectors.toList());
             writer.writeList(subItemSummaryListWriter, summaries, ctxt);
         }
     }
