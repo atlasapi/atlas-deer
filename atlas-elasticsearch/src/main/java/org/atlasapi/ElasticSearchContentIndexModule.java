ff8d5f05055eef546ef7356bb82ab90444c7c560
Jamie Perkins <jamie@metabroadcast>
2015-09-07 16:59:25 +0100

    Wire up equivalent content index

--- atlas-elasticsearch/src/main/java/org/atlasapi/ElasticSearchContentIndexModule.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/ElasticSearchContentIndexModule.java
@@ -5,12 +5,15 @@ import java.util.concurrent.TimeUnit;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.base.Splitter;
 import org.atlasapi.channel.ChannelGroupResolver;
+import org.atlasapi.content.ContentIndex;
 import org.atlasapi.content.ContentResolver;
 import org.atlasapi.content.EsContentIndex;
 import org.atlasapi.content.EsContentTitleSearcher;
 import org.atlasapi.content.InstrumentedEsContentIndex;
+import org.atlasapi.content.PseudoEquivalentEsContentIndex;
 import org.atlasapi.topic.EsPopularTopicIndex;
 import org.atlasapi.topic.EsTopicIndex;
+import org.atlasapi.util.SecondaryIndex;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -29,12 +32,15 @@ public class ElasticSearchContentIndexModule implements IndexModule {
 
     private final Logger log = LoggerFactory.getLogger(ElasticSearchContentIndexModule.class);
 
-    private final EsContentIndex contentIndex;
+    private final EsContentIndex unequivIndex;
+    private final ContentIndex equivIndex;
     private final EsTopicIndex topicIndex;
     private final EsPopularTopicIndex popularTopicsIndex;
     private final EsContentTitleSearcher contentSearcher;
 
-    public ElasticSearchContentIndexModule(String seeds, String clusterName, String indexName, long requestTimeout, ContentResolver resolver, MetricRegistry metrics, ChannelGroupResolver channelGroupResolver) {
+    public ElasticSearchContentIndexModule(String seeds, String clusterName, String indexName, long requestTimeout,
+            ContentResolver resolver, MetricRegistry metrics, ChannelGroupResolver channelGroupResolver,
+            SecondaryIndex equivalentContentIndex) {
         Settings settings = ImmutableSettings.settingsBuilder()
                 .put("client.transport.sniff", true)
                 .put("cluster.name", clusterName)
@@ -43,7 +49,13 @@ public class ElasticSearchContentIndexModule implements IndexModule {
         for (String host : Splitter.on(",").splitToList(seeds)) {
             client.addTransportAddress(new InetSocketTransportAddress(host, 9300));
         }
-        this.contentIndex = new InstrumentedEsContentIndex(client, indexName, requestTimeout, resolver, metrics, channelGroupResolver);
+
+        unequivIndex = new EsContentIndex(client, indexName, requestTimeout, resolver, channelGroupResolver);
+
+        PseudoEquivalentEsContentIndex equivalentEsIndex =
+                new PseudoEquivalentEsContentIndex(unequivIndex, equivalentContentIndex);
+
+        this.equivIndex = new InstrumentedEsContentIndex(equivalentEsIndex, metrics);
         this.popularTopicsIndex = new EsPopularTopicIndex(client);
         this.topicIndex = new EsTopicIndex(client, EsSchema.TOPICS_INDEX, 60, TimeUnit.SECONDS);
         this.contentSearcher = new EsContentTitleSearcher(client);
@@ -51,7 +63,7 @@ public class ElasticSearchContentIndexModule implements IndexModule {
 
     public void init() {
         //Investigate service manager?
-        Futures.addCallback(contentIndex.start(), new FutureCallback<State>() {
+        Futures.addCallback(unequivIndex.start(), new FutureCallback<State>() {
 
             @Override
             public void onSuccess(State result) {
@@ -77,10 +89,14 @@ public class ElasticSearchContentIndexModule implements IndexModule {
         });
     }
 
-    public EsContentIndex contentIndex() {
-        return contentIndex;
+    public ContentIndex equivContentIndex() {
+        return equivIndex;
     }
-    
+
+    public ContentIndex unequivContentIndex() {
+        return unequivIndex;
+    }
+
     public EsTopicIndex topicIndex() {
         return topicIndex;
     }
