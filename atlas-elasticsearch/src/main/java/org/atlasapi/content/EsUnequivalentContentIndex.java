6e85d0e8b37cffc422d8dbdd98ffc52051120cbf
Jamie Perkins <jamie@metabroadcast>
2015-09-10 10:57:09 +0100

    Split out ES filters into static factories

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/EsUnequivalentContentIndex.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/EsUnequivalentContentIndex.java
@@ -2,51 +2,35 @@ package org.atlasapi.content;
 
 import com.google.common.base.Function;
 import com.google.common.base.Objects;
-import com.google.common.base.Throwables;
 import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import com.metabroadcast.common.query.Selection;
-import org.atlasapi.channel.ChannelGroup;
 import org.atlasapi.channel.ChannelGroupResolver;
-import org.atlasapi.channel.ChannelNumbering;
 import org.atlasapi.criteria.AttributeQuerySet;
 import org.atlasapi.entity.Id;
-import org.atlasapi.entity.util.Resolved;
 import org.atlasapi.media.entity.Publisher;
-import org.atlasapi.topic.EsTopic;
 import org.atlasapi.util.ElasticsearchIndexCreator;
 import org.atlasapi.util.EsQueryBuilder;
 import org.atlasapi.util.FiltersBuilder;
 import org.atlasapi.util.FutureSettingActionListener;
-import org.atlasapi.util.ImmutableCollectors;
 import org.atlasapi.util.SecondaryIndex;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.index.query.AndFilterBuilder;
 import org.elasticsearch.index.query.BoolFilterBuilder;
-import org.elasticsearch.index.query.FilterBuilder;
 import org.elasticsearch.index.query.FilterBuilders;
-import org.elasticsearch.index.query.NestedFilterBuilder;
-import org.elasticsearch.index.query.OrFilterBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeFilterBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -60,7 +44,8 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
         Long id = hit.field(EsContent.CANONICAL_ID).<Number>value().longValue();
         return Id.valueOf(id);
     };
-    public static final Function<SearchHit, Id> SEARCH_HIT_ID_FUNCTION = hit -> Id.valueOf(hit.field(EsContent.ID).<Number>value().longValue());
+    public static final Function<SearchHit, Id> SEARCH_HIT_ID_FUNCTION = hit ->
+            Id.valueOf(hit.field(EsContent.ID).<Number>value().longValue());
     public static final Function<SearchHit, Id> HIT_TO_ID = SEARCH_HIT_ID_FUNCTION;
 
     private final Logger log = LoggerFactory.getLogger(EsUnequivalentContentIndex.class);
@@ -76,8 +61,14 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
     private final SecondaryIndex equivIdIndex;
     private final EsUnequivalentContentIndexer indexer;
 
-    public EsUnequivalentContentIndex(Client esClient, String indexName, ContentResolver resolver,
-            ChannelGroupResolver channelGroupResolver, SecondaryIndex equivIdIndex, Integer requestTimeout) {
+    public EsUnequivalentContentIndex(
+            Client esClient,
+            String indexName,
+            ContentResolver resolver,
+            ChannelGroupResolver channelGroupResolver,
+            SecondaryIndex equivIdIndex,
+            Integer requestTimeout
+    ) {
         this.esClient = checkNotNull(esClient);
         this.index = checkNotNull(indexName);
         this.channelGroupResolver = checkNotNull(channelGroupResolver);
@@ -147,11 +138,20 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
             }
 
             if (queryParams.get().getBrandId().isPresent()) {
-                filterBuilder.must(getBrandIdFilter(queryParams.get().getBrandId().get()));
+                filterBuilder.must(
+                        FiltersBuilder.getBrandIdFilter(
+                                queryParams.get().getBrandId().get(),
+                                equivIdIndex
+                        )
+                );
             }
 
             if (queryParams.get().getSeriesId().isPresent()) {
-                filterBuilder.must(getSeriesIdFilter(queryParams.get().getSeriesId().get()));
+                filterBuilder.must(
+                        FiltersBuilder.getSeriesIdFilter(
+                                queryParams.get().getSeriesId().get(), equivIdIndex
+                        )
+                );
             }
 
             if (queryParams.get().getOrdering().isPresent()) {
@@ -159,16 +159,22 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
             }
 
             if (queryParams.get().getTopicFilterIds().isPresent()) {
-                filterBuilder.must(getTopicIdFilters(queryParams.get().getTopicFilterIds().get()));
+                filterBuilder.must(
+                        FiltersBuilder.buildTopicIdFilter(queryParams.get().getTopicFilterIds().get())
+                );
             }
 
             if (queryParams.get().getActionableFilterParams().isPresent()) {
-                filterBuilder.must(getActionableFilter(queryParams.get().getActionableFilterParams().get()));
+                filterBuilder.must(FiltersBuilder.builderActionableFilter(
+                        queryParams.get().getActionableFilterParams().get())
+                );
             }
 
             /* Temporarily disabled due to not working correctly
             if (queryParams.get().getRegionId().isPresent()) {
-                queryBuilderFactory = addRegionFilter(queryParams, queryBuilderFactory);
+                filterBuilder.must(
+                        FiltersBuilder.addRegionFilter(queryParams, channelGroupResolver);
+                )
             }
             */
 
@@ -200,99 +206,6 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
         });
     }
 
-    private FilterBuilder getSeriesIdFilter(Id id) {
-        try {
-            ImmutableSet<Long> ids = Futures.get(equivIdIndex.reverseLookup(id), IOException.class);
-            return FilterBuilders.termsFilter(EsContent.SERIES, ids);
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
-    }
-
-    private FilterBuilder getActionableFilter(Map<String, String> actionableParams) {
-        OrFilterBuilder orFilterBuilder = FilterBuilders.orFilter();
-        if (actionableParams.get("location.available") != null) {
-            orFilterBuilder.add(getAvailabilityFilter());
-        }
-        DateTime broadcastTimeGreaterThan = actionableParams.get("broadcast.time.gt") == null ? null
-                : DateTime.parse(actionableParams.get("broadcast.time.gt"));
-        DateTime broadcastTimeLessThan = actionableParams.get("broadcast.time.lt") == null ? null
-                : DateTime.parse(actionableParams.get("broadcast.time.lt"));
-        if (broadcastTimeGreaterThan != null || broadcastTimeLessThan != null) {
-            orFilterBuilder.add(
-                    broadcastRangeFilterFrom(broadcastTimeGreaterThan, broadcastTimeLessThan)
-            );
-        }
-        return orFilterBuilder;
-    }
-
-    private FilterBuilder broadcastRangeFilterFrom(DateTime broadcastTimeGreaterThan, DateTime broadcastTimeLessThan) {
-        RangeFilterBuilder parentFilter = FilterBuilders.rangeFilter("transmissionTimeInMillis");
-        if (broadcastTimeGreaterThan != null) {
-            parentFilter.gte(broadcastTimeGreaterThan.getMillis());
-        }
-        if (broadcastTimeLessThan != null) {
-            parentFilter.lte(broadcastTimeLessThan.getMillis());
-        }
-        return FilterBuilders.orFilter(
-                parentFilter,
-                FilterBuilders.hasChildFilter(EsContent.CHILD_ITEM, parentFilter)
-        );
-    }
-
-    private FilterBuilder getBrandIdFilter(Id id) {
-        try {
-            ImmutableSet<Long> ids = Futures.get(equivIdIndex.reverseLookup(id), IOException.class);
-            return FilterBuilders.termsFilter(EsContent.BRAND, ids);
-        } catch (IOException ioe) {
-            throw Throwables.propagate(ioe);
-        }
-    }
-
-    private FilterBuilder getAvailabilityFilter() {
-        NestedFilterBuilder rangeFilter = FilterBuilders.nestedFilter(
-                EsContent.LOCATIONS,
-                FilterBuilders.andFilter(
-                        FilterBuilders.rangeFilter(EsLocation.AVAILABILITY_TIME)
-                                .lte(DateTime.now().toString()),
-                        FilterBuilders.rangeFilter(EsLocation.AVAILABILITY_END_TIME)
-                                .gte(DateTime.now().toString()))
-        );
-        return FilterBuilders.orFilter(
-                rangeFilter,
-                FilterBuilders.hasChildFilter(EsContent.CHILD_ITEM, rangeFilter)
-        );
-    }
-
-    public FilterBuilder getTopicIdFilters(List<List<InclusionExclusionId>> topicIdSets) {
-        ImmutableList.Builder<FilterBuilder> topicIdFilters = ImmutableList.builder();
-        for (List<InclusionExclusionId> idSet : topicIdSets) {
-            BoolFilterBuilder filterForThisSet = FilterBuilders.boolFilter();
-            for (InclusionExclusionId id : idSet) {
-                addFilterForTopicId(filterForThisSet, id);
-            }
-            topicIdFilters.add(filterForThisSet);
-        }
-        AndFilterBuilder andFilterBuilder = FilterBuilders.andFilter();
-        topicIdFilters.build().forEach(andFilterBuilder::add);
-        return andFilterBuilder;    
-    }
-
-    private void addFilterForTopicId(BoolFilterBuilder filterBuilder, InclusionExclusionId id) {
-        NestedFilterBuilder filterForId = FilterBuilders.nestedFilter(
-                EsContent.TOPICS + "." + EsTopic.TYPE_NAME,
-                FilterBuilders.termFilter(
-                        EsContent.TOPICS + "." + EsTopic.TYPE_NAME + "." + EsContent.ID,
-                        id.getId()
-                )
-        );
-        if (id.isExcluded()) {
-            filterBuilder.mustNot(filterForId);
-        } else {
-            filterBuilder.must(filterForId);
-        }
-    }
-
     private void addSortOrder(Optional<IndexQueryParams> queryParams, SearchRequestBuilder reqBuilder) {
         QueryOrdering order = queryParams.get().getOrdering().get();
         if ("relevance".equalsIgnoreCase(order.getPath())) {
@@ -320,29 +233,5 @@ public class EsUnequivalentContentIndex extends AbstractIdleService implements C
                 .must(queryBuilder)
                 .must(TitleQueryBuilder.build(searchParams.getSearchTerm(), searchParams.getBoost().orElse(5F)));
         return queryBuilder;
-    }
-
-    @SuppressWarnings("unchecked")
-    private FilterBuilder addRegionFilter(Optional<IndexQueryParams> queryParams) {
-        Id regionId = queryParams.get().getRegionId().get();
-        ChannelGroup region;
-        try {
-            Resolved<ChannelGroup<?>> resolved = Futures.get(
-                    channelGroupResolver.resolveIds(ImmutableList.of(regionId)), IOException.class
-            );
-            region = resolved.getResources().first().get();
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
-
-        ImmutableList<ChannelNumbering> channels = ImmutableList.copyOf(region.<ChannelNumbering>getChannels());
-        ImmutableList<Long> channelsIdsForRegion = channels.stream()
-                .map(c -> c.getChannel().getId())
-                .map(Id::longValue)
-                .collect(ImmutableCollectors.toList());
-
-        return FilterBuilders.termsFilter(
-                EsContent.BROADCASTS + "." + EsBroadcast.CHANNEL, channelsIdsForRegion
-        );
     }
 }
--- atlas-elasticsearch/src/main/java/org/atlasapi/util/FiltersBuilder.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/util/FiltersBuilder.java
@@ -1,13 +1,38 @@
 package org.atlasapi.util;
 
-import org.atlasapi.content.EsContent;
-import org.atlasapi.content.Specialization;
-import org.atlasapi.media.entity.Publisher;
-import org.elasticsearch.index.query.FilterBuilders;
-import org.elasticsearch.index.query.TermsFilterBuilder;
-
 import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.Futures;
+import org.atlasapi.channel.ChannelGroup;
+import org.atlasapi.channel.ChannelGroupResolver;
+import org.atlasapi.channel.ChannelNumbering;
+import org.atlasapi.content.EsBroadcast;
+import org.atlasapi.content.EsContent;
+import org.atlasapi.content.EsLocation;
+import org.atlasapi.content.InclusionExclusionId;
+import org.atlasapi.content.IndexQueryParams;
+import org.atlasapi.content.Specialization;
+import org.atlasapi.entity.Id;
+import org.atlasapi.entity.util.Resolved;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.topic.EsTopic;
+import org.elasticsearch.index.query.AndFilterBuilder;
+import org.elasticsearch.index.query.BoolFilterBuilder;
+import org.elasticsearch.index.query.FilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.NestedFilterBuilder;
+import org.elasticsearch.index.query.OrFilterBuilder;
+import org.elasticsearch.index.query.RangeFilterBuilder;
+import org.elasticsearch.index.query.TermsFilterBuilder;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class FiltersBuilder {
 
@@ -16,12 +41,126 @@ public class FiltersBuilder {
     }
 
     public static TermsFilterBuilder buildForSpecializations(Iterable<Specialization> specializations) {
-        return FilterBuilders.termsFilter(EsContent.SPECIALIZATION, Iterables.transform(specializations, new Function<Specialization, String>() {
+        return FilterBuilders.termsFilter(
+                EsContent.SPECIALIZATION,
+                Iterables.transform(specializations, input -> input.name())
+        );
+    }
 
-            @Override
-            public String apply(Specialization input) {
-                return input.name();
+    public static FilterBuilder buildTopicIdFilter(List<List<InclusionExclusionId>> topicIdSets) {
+        ImmutableList.Builder<FilterBuilder> topicIdFilters = ImmutableList.builder();
+        for (List<InclusionExclusionId> idSet : topicIdSets) {
+            BoolFilterBuilder filterForThisSet = FilterBuilders.boolFilter();
+            for (InclusionExclusionId id : idSet) {
+                addFilterForTopicId(filterForThisSet, id);
             }
-        }));
+            topicIdFilters.add(filterForThisSet);
+        }
+        AndFilterBuilder andFilterBuilder = FilterBuilders.andFilter();
+        topicIdFilters.build().forEach(andFilterBuilder::add);
+        return andFilterBuilder;
+    }
+
+    private static void addFilterForTopicId(BoolFilterBuilder filterBuilder, InclusionExclusionId id) {
+        NestedFilterBuilder filterForId = FilterBuilders.nestedFilter(
+                EsContent.TOPICS + "." + EsTopic.TYPE_NAME,
+                FilterBuilders.termFilter(
+                        EsContent.TOPICS + "." + EsTopic.TYPE_NAME + "." + EsContent.ID,
+                        id.getId()
+                )
+        );
+        if (id.isExcluded()) {
+            filterBuilder.mustNot(filterForId);
+        } else {
+            filterBuilder.must(filterForId);
+        }
+    }
+
+    public static FilterBuilder buildAvailabilityFilter() {
+        NestedFilterBuilder rangeFilter = FilterBuilders.nestedFilter(
+                EsContent.LOCATIONS,
+                FilterBuilders.andFilter(
+                        FilterBuilders.rangeFilter(EsLocation.AVAILABILITY_TIME)
+                                .lte(DateTime.now().toString()),
+                        FilterBuilders.rangeFilter(EsLocation.AVAILABILITY_END_TIME)
+                                .gte(DateTime.now().toString()))
+        );
+        return FilterBuilders.orFilter(
+                rangeFilter,
+                FilterBuilders.hasChildFilter(EsContent.CHILD_ITEM, rangeFilter)
+        );
+    }
+
+    public static FilterBuilder builderActionableFilter(Map<String, String> actionableParams) {
+        OrFilterBuilder orFilterBuilder = FilterBuilders.orFilter();
+        if (actionableParams.get("location.available") != null) {
+            orFilterBuilder.add(buildAvailabilityFilter());
+        }
+        DateTime broadcastTimeGreaterThan = actionableParams.get("broadcast.time.gt") == null ? null
+                : DateTime.parse(actionableParams.get("broadcast.time.gt"));
+        DateTime broadcastTimeLessThan = actionableParams.get("broadcast.time.lt") == null ? null
+                : DateTime.parse(actionableParams.get("broadcast.time.lt"));
+        if (broadcastTimeGreaterThan != null || broadcastTimeLessThan != null) {
+            orFilterBuilder.add(
+                    buildBroadcastRangeFilter(broadcastTimeGreaterThan, broadcastTimeLessThan)
+            );
+        }
+        return orFilterBuilder;
+    }
+
+    public static FilterBuilder buildBroadcastRangeFilter(DateTime broadcastTimeGreaterThan, DateTime broadcastTimeLessThan) {
+        RangeFilterBuilder parentFilter = FilterBuilders.rangeFilter("transmissionTimeInMillis");
+        if (broadcastTimeGreaterThan != null) {
+            parentFilter.gte(broadcastTimeGreaterThan.getMillis());
+        }
+        if (broadcastTimeLessThan != null) {
+            parentFilter.lte(broadcastTimeLessThan.getMillis());
+        }
+        return FilterBuilders.orFilter(
+                parentFilter,
+                FilterBuilders.hasChildFilter(EsContent.CHILD_ITEM, parentFilter)
+        );
+    }
+
+    public static FilterBuilder getSeriesIdFilter(Id id, SecondaryIndex equivIdIndex) {
+        try {
+            ImmutableSet<Long> ids = Futures.get(equivIdIndex.reverseLookup(id), IOException.class);
+            return FilterBuilders.termsFilter(EsContent.SERIES, ids);
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    public static FilterBuilder getBrandIdFilter(Id id, SecondaryIndex equivIdIndex) {
+        try {
+            ImmutableSet<Long> ids = Futures.get(equivIdIndex.reverseLookup(id), IOException.class);
+            return FilterBuilders.termsFilter(EsContent.BRAND, ids);
+        } catch (IOException ioe) {
+            throw Throwables.propagate(ioe);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static FilterBuilder addRegionFilter(Optional<IndexQueryParams> queryParams, ChannelGroupResolver channelGroupResolver) {
+        Id regionId = queryParams.get().getRegionId().get();
+        ChannelGroup region;
+        try {
+            Resolved<ChannelGroup<?>> resolved = Futures.get(
+                    channelGroupResolver.resolveIds(ImmutableList.of(regionId)), IOException.class
+            );
+            region = resolved.getResources().first().get();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+
+        ImmutableList<ChannelNumbering> channels = ImmutableList.copyOf(region.<ChannelNumbering>getChannels());
+        ImmutableList<Long> channelsIdsForRegion = channels.stream()
+                .map(c -> c.getChannel().getId())
+                .map(Id::longValue)
+                .collect(ImmutableCollectors.toList());
+
+        return FilterBuilders.termsFilter(
+                EsContent.BROADCASTS + "." + EsBroadcast.CHANNEL, channelsIdsForRegion
+        );
     }
 }
