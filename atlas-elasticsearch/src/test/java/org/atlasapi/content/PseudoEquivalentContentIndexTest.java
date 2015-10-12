77dfdde42f73b9ff9e32308fff4e5cd4a899c22f
Jamie <jamie@metabroadcast.com>
2015-10-12 17:28:35 +0100

    Test to ensure content that is not actively published is removed

--- atlas-elasticsearch/src/test/java/org/atlasapi/content/PseudoEquivalentContentIndexTest.java
+++ atlas-elasticsearch/src/test/java/org/atlasapi/content/PseudoEquivalentContentIndexTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 import org.atlasapi.EsSchema;
 import org.atlasapi.channel.ChannelGroupResolver;
@@ -17,6 +18,8 @@ import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.util.CassandraSecondaryIndex;
 import org.atlasapi.util.ElasticSearchHelper;
 import org.atlasapi.util.SecondaryIndex;
+import org.elasticsearch.action.get.GetRequestBuilder;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.Client;
 import org.junit.Before;
 import org.junit.Test;
@@ -80,6 +83,38 @@ public class PseudoEquivalentContentIndexTest {
         ImmutableList<Id> ids = ImmutableList.copyOf(Futures.get(result, IOException.class).getIds());
 
         assertThat(ids, is(ImmutableList.of(Id.valueOf(2l), Id.valueOf(3l))));
+    }
+
+    @Test
+    public void testRemovesNonActivelyPublishedContent() throws IndexException, ExecutionException, InterruptedException {
+        when(equivIndex.lookup(anyList()))
+                .thenReturn(Futures.immediateFuture(ImmutableMap.of(10l, 10l)))
+                .thenReturn(Futures.immediateFuture(ImmutableMap.of(20l, 20l)))
+                .thenReturn(Futures.immediateFuture(ImmutableMap.of(3l, 3l)))
+                .thenReturn(
+                        Futures.immediateFuture(
+                                ImmutableMap.of(
+                                        1l, 2l,
+                                        2l, 2l,
+                                        3l, 3l
+                                )
+                        )
+                );
+
+        Item item = new Item(Id.valueOf(10l), Publisher.METABROADCAST);
+        indexAndRefresh(item);
+        item.setActivelyPublished(false);
+        indexAndRefresh(item);
+        GetResponse resp = esNode.prepareGet().setIndex("content").setId("10").execute().get();
+        assertThat(resp.isExists(), is(false));
+
+        Episode episode = new Episode(Id.valueOf(20l), Publisher.METABROADCAST);
+        episode.setContainerRef(new BrandRef(Id.valueOf(30l), Publisher.METABROADCAST));
+        indexAndRefresh(episode);
+        episode.setActivelyPublished(false);
+        indexAndRefresh(episode);
+        GetResponse resp2 = esNode.prepareGet().setIndex("content").setId("20").execute().get();
+        assertThat(resp2.isExists(), is(false));
     }
 
     private void indexAndRefresh(Content... contents) throws IndexException {
