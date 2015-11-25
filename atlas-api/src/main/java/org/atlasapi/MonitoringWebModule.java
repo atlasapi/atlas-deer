8f6fb460d7a273a37727beb55441a674619762c3
Thomas Maroulis <thomas@metabroadcast.com>
2015-11-25 17:12:31 +0000

    Remove imports from MonitoringWebModule
    - These should not be required since we are not using any of these
    beans here. They are also breaking the code since the monitoring
    component has no access to the configuration and so can't instantiate
    AtlasPersistenceModule

--- atlas-api/src/main/java/org/atlasapi/MonitoringWebModule.java
+++ atlas-api/src/main/java/org/atlasapi/MonitoringWebModule.java
@@ -5,20 +5,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 
-import org.atlasapi.system.HealthModule;
 import org.atlasapi.system.JettyHealthProbe;
-import org.atlasapi.system.MetricsModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({
-        HealthModule.class,
-        MetricsModule.class,
-        AtlasPersistenceModule.class
-})
 public class MonitoringWebModule {
 
     @Autowired
