87fc0807a7954c29dab173e445f97d8bf6ab1607
Dias Saparov <dias@metabroadcast.com>
2016-01-20 17:57:09 +0000

    fix test for organisation resolver

--- atlas-legacy/src/test/java/org/atlasapi/system/legacy/LegacyOrganisationResolverTest.java
+++ atlas-legacy/src/test/java/org/atlasapi/system/legacy/LegacyOrganisationResolverTest.java
@@ -1,5 +1,6 @@
 package org.atlasapi.system.legacy;
 
+import org.atlasapi.entity.Alias;
 import org.atlasapi.entity.Id;
 import org.atlasapi.entity.util.Resolved;
 import org.atlasapi.media.entity.Organisation;
@@ -11,10 +12,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.sameInstance;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.FluentIterable;
@@ -25,23 +28,26 @@ import com.google.common.util.concurrent.ListenableFuture;
 public class LegacyOrganisationResolverTest {
     private @Mock OrganisationStore store;
     private @Mock LegacyOrganisationTransformer transformer;
-    private @Mock Organisation legacyOrganisation;
-    private @Mock org.atlasapi.organisation.Organisation organisation;
+    private Organisation legacyOrganisation;
+    private org.atlasapi.organisation.Organisation organisation;
 
     private LegacyOrganisationResolver resolver;
 
     @Before
     public void setUp() {
+        transformer =  new LegacyOrganisationTransformer();
         resolver = new LegacyOrganisationResolver(store, transformer);
     }
 
     @Test
     public void testResolvingOrganisations() throws Throwable {
-        when(transformer.apply(legacyOrganisation)).thenReturn(organisation);
+        organisation = new org.atlasapi.organisation.Organisation();
+        legacyOrganisation = new Organisation();
+        legacyOrganisation.setAlternativeTitles(ImmutableList.of("title1", "title2"));
         when(store.organisation(1l)).thenReturn(Optional.of(legacyOrganisation));
         ListenableFuture<Resolved<org.atlasapi.organisation.Organisation>> resources = resolver.resolveIds(ImmutableList.of(Id.valueOf(1L)));
         FluentIterable<org.atlasapi.organisation.Organisation> organisations = resources.get().getResources();
         assertThat(organisations.size(), is(1));
-        assertThat(organisations.first().get(), sameInstance(organisation));
+        assertThat(organisations.first().get().getAlternativeTitles(), is(legacyOrganisation.getAlternativeTitles()));
     }
 }
