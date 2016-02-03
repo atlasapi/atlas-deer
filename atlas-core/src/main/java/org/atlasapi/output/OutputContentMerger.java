c183cef3d47936daaaba139bc5bb7a8057dfdc48
Timothy Sum Hon Mun <timothy@metabroadcast.com>
2016-02-03 11:50:01 +0000

    updated OutputContentMerger and its test for generic images merging

--- atlas-core/src/main/java/org/atlasapi/output/OutputContentMerger.java
+++ atlas-core/src/main/java/org/atlasapi/output/OutputContentMerger.java
@@ -38,6 +38,7 @@ import org.atlasapi.entity.Person;
 import org.atlasapi.entity.Sourced;
 import org.atlasapi.equivalence.EquivalenceRef;
 import org.atlasapi.equivalence.SeriesOrder;
+import org.atlasapi.media.entity.ImageType;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.segment.SegmentEvent;
 import org.joda.time.Duration;
@@ -314,7 +315,7 @@ public class OutputContentMerger implements EquivalentsMergeStrategy<Content> {
     private <T extends Described> void applyImagePrefs(ApplicationSources sources, T chosen, Iterable<T> notChosen) {
         Iterable<T> all = Iterables.concat(ImmutableList.of(chosen), notChosen);
         if (sources.imagePrecedenceEnabled()) {
-            List<T> topImageMatches = sources.getSourcedImagePrecedenceOrdering().leastOf(Iterables.filter(all, HAS_AVAILABLE_IMAGE_SET), 1);
+            List<T> topImageMatches = sources.getSourcedImagePrecedenceOrdering().leastOf(Iterables.filter(all, HAS_AVAILABLE_AND_NOT_GENERIC_IMAGE_SET), 1);
             if (!topImageMatches.isEmpty()) {
                 T top = topImageMatches.get(0);
                 top.getImages().forEach(img -> img.setSource(top.getSource()));
@@ -471,14 +472,14 @@ public class OutputContentMerger implements EquivalentsMergeStrategy<Content> {
         }
     }
 
-    private static final Predicate<Described> HAS_AVAILABLE_IMAGE_SET = new Predicate<Described>() {
+    private static final Predicate<Described> HAS_AVAILABLE_AND_NOT_GENERIC_IMAGE_SET = new Predicate<Described>() {
 
         @Override
         public boolean apply(Described content) {
             if (content.getImage() == null) {
                 return false;
             }
-            return isImageAvailable(content.getImage(), content.getImages());
+            return isImageAvailableAndNotGeneric(content.getImage(), content.getImages());
         }
     };
     
@@ -490,7 +491,7 @@ public class OutputContentMerger implements EquivalentsMergeStrategy<Content> {
         }
     };
 
-    private static boolean isImageAvailable(String imageUri, Iterable<Image> images) {
+    private static boolean isImageAvailableAndNotGeneric(String imageUri, Iterable<Image> images) {
 
         // Fneh. Image URIs differ between the image attribute and the canonical URI on Images.
         // See PaProgrammeProcessor for why.
@@ -500,7 +501,7 @@ public class OutputContentMerger implements EquivalentsMergeStrategy<Content> {
         // If there is a corresponding Image object for this URI, we check its availability
         for (Image image : images) {
             if (image.getCanonicalUri().equals(rewrittenUri)) {
-               return Image.IS_AVAILABLE.apply(image);
+               return Image.IS_AVAILABLE.apply(image) && !ImageType.GENERIC.equals(image.getType());
             }
         }
         // Otherwise, we can only assume the image is available as we know no better
