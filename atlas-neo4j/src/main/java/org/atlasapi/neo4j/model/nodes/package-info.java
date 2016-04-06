d54418b4174af180aa65312596d820fda92f761f
Thomas Maroulis <thomas@metabroadcast.com>
2016-04-06 18:04:10 +0100

    Rebase in master and add NonNullByDefault annotation

---
+++ atlas-neo4j/src/main/java/org/atlasapi/neo4j/model/nodes/package-info.java
@@ -0,0 +1,4 @@
+@NonNullByDefault
+package org.atlasapi.neo4j.model.nodes;
+
+import com.metabroadcast.common.annotation.NonNullByDefault;
---
+++ atlas-neo4j/src/main/java/org/atlasapi/neo4j/service/package-info.java
@@ -0,0 +1,4 @@
+@NonNullByDefault
+package org.atlasapi.neo4j.service;
+
+import com.metabroadcast.common.annotation.NonNullByDefault;
