c94f45a6d47343b9f21da44ff41e9f66f74a966e
Thomas Maroulis <thomas@metabroadcast.com>
2016-05-18 18:02:59 +0100

    Revert "Change equivalence graph worker to not retry failed messages"
    This reverts commit e003a82755fdf9f8d6f8c1a1f55a0e4fb9614f84.

--- atlas-processing/src/main/java/org/atlasapi/messaging/WorkersModule.java
+++ atlas-processing/src/main/java/org/atlasapi/messaging/WorkersModule.java
@@ -282,7 +282,7 @@ public class WorkersModule {
                 .withProducerSystem(equivSystem)
                 .withDefaultConsumers(contentEquivalenceGraphChangesNumOfConsumers)
                 .withMaxConsumers(contentEquivalenceGraphChangesNumOfConsumers)
-                .withFailedMessagePersistence(persistence.databasedWriteMongo())
+                .withPersistentRetryPolicy(persistence.databasedWriteMongo())
                 .build();
     }
 
