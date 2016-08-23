ccd4e9259f5cb0f408d8c88bc3e41241d67f7328
Emils Solmanis <emils@metabroadcast.com>
2016-08-23 18:11:35 +0100

    Add package-info.java's to CQL store packages

---
+++ atlas-cassandra/src/main/java/org/atlasapi/content/v2/model/package-info.java
@@ -0,0 +1,3 @@
+@NonNullByDefault package org.atlasapi.content.v2.model;
+
+import com.metabroadcast.common.annotation.NonNullByDefault;
---
+++ atlas-cassandra/src/main/java/org/atlasapi/content/v2/model/udt/package-info.java
@@ -0,0 +1,3 @@
+@NonNullByDefault package org.atlasapi.content.v2.model.udt;
+
+import com.metabroadcast.common.annotation.NonNullByDefault;
