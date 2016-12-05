fecda1a9cb62c0ad40e16410d7619edbcaae0d5f
Thomas Maroulis <thomas@metabroadcast.com>
2016-12-05 14:59:05 +0000

    Remove unused scheduler code

--- atlas-processing/src/main/java/org/atlasapi/system/bootstrap/BootstrapModule.java
+++ atlas-processing/src/main/java/org/atlasapi/system/bootstrap/BootstrapModule.java
@@ -1,19 +1,11 @@
 package org.atlasapi.system.bootstrap;
 
-import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.PostConstruct;
-
 import org.atlasapi.AtlasPersistenceModule;
 import org.atlasapi.ElasticSearchContentIndexModule;
-import org.atlasapi.SchedulerModule;
-import org.atlasapi.content.Content;
-import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.messaging.MessagingModule;
 import org.atlasapi.system.MetricsModule;
 import org.atlasapi.system.ProcessingHealthModule;
@@ -22,17 +14,10 @@ import org.atlasapi.system.bootstrap.workers.DelegatingContentStore;
 import org.atlasapi.system.bootstrap.workers.DirectAndExplicitEquivalenceMigrator;
 import org.atlasapi.system.legacy.MongoProgressStore;
 import org.atlasapi.system.legacy.ProgressStore;
-import org.atlasapi.topic.Topic;
 
 import com.metabroadcast.common.properties.Configurer;
-import com.metabroadcast.common.scheduling.RepetitionRules;
-import com.metabroadcast.common.scheduling.UpdateProgress;
-import com.metabroadcast.common.time.DayRangeGenerator;
-import com.metabroadcast.common.time.SystemClock;
 
 import com.codahale.metrics.MetricRegistry;
-import com.google.common.base.Supplier;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
@@ -41,13 +26,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@SuppressWarnings("PublicConstructor")
 @Configuration
 @Import({
-        AtlasPersistenceModule.class, BootstrapWorkersModule.class,
-        SchedulerModule.class, ProcessingHealthModule.class })
+        AtlasPersistenceModule.class,
+        BootstrapWorkersModule.class,
+        ProcessingHealthModule.class
+})
 public class BootstrapModule {
 
-    //we only need 2 here, on to run the bootstrap and one to be able to return quickly when it's running
+    // We only need 2 here, one to run the bootstrap and one to be able to return quickly
+    // when it's running
     private static final Integer NUMBER_OF_SCHECHULE_CONTROLLER_THREADS = 2;
     private static final Integer NUMBER_OF_SCHEDULE_BOOTSTRAP_THREADS = Configurer.get(
             "boootstrap.schedule.numThreads").toInt();
@@ -56,7 +45,6 @@ public class BootstrapModule {
 
     @Autowired private AtlasPersistenceModule persistence;
     @Autowired private BootstrapWorkersModule workers;
-    @Autowired private SchedulerModule scheduler;
     @Autowired private ElasticSearchContentIndexModule search;
     @Autowired private MetricRegistry metrics;
     @Autowired private DirectAndExplicitEquivalenceMigrator explicitEquivalenceMigrator;
@@ -70,31 +58,21 @@ public class BootstrapModule {
         BootstrapController bootstrapController = new BootstrapController();
 
         bootstrapController.addBootstrapPair("legacy-content",
-                new ResourceBootstrapper<Content>(persistence.legacyContentLister()),
-                new BootstrapListenerFactory<Content>() {
-
-                    @Override
-                    public BootstrapListener<Content> buildWithConcurrency(int concurrencyLevel) {
-                        return new ContentWritingBootstrapListener(
-                                concurrencyLevel,
-                                persistence.contentStore()
-                        );
-                    }
-                }
+                new ResourceBootstrapper<>(persistence.legacyContentLister()),
+                concurrencyLevel -> new ContentWritingBootstrapListener(
+                        concurrencyLevel,
+                        persistence.contentStore()
+                )
         );
+
         bootstrapController.addBootstrapPair("legacy-topics",
-                new ResourceBootstrapper<Topic>(persistence.legacyTopicLister()),
-                new BootstrapListenerFactory<Topic>() {
-
-                    @Override
-                    public BootstrapListener<Topic> buildWithConcurrency(int concurrencyLevel) {
-                        return new TopicWritingBootstrapListener(
-                                concurrencyLevel,
-                                persistence.topicStore()
-                        );
-                    }
-                }
+                new ResourceBootstrapper<>(persistence.legacyTopicLister()),
+                concurrencyLevel -> new TopicWritingBootstrapListener(
+                        concurrencyLevel,
+                        persistence.topicStore()
+                )
         );
+
         return bootstrapController;
     }
 
@@ -142,35 +120,6 @@ public class BootstrapModule {
     }
 
     @Bean
-    ExecutorServiceScheduledTask<UpdateProgress> scheduleBootstrapTask() {
-        ChannelIntervalScheduleBootstrapTaskFactory taskFactory = workers.scheduleBootstrapTaskFactory();
-        DayRangeGenerator dayRangeGenerator = new DayRangeGenerator().withLookAhead(7)
-                .withLookBack(7);
-        Set<Publisher> sources = ImmutableSet.of(Publisher.PA);
-        Supplier<Iterable<ChannelIntervalScheduleBootstrapTask>> supplier =
-                new SourceChannelIntervalTaskSupplier<ChannelIntervalScheduleBootstrapTask>(
-                        taskFactory,
-                        persistence.channelResolver(),
-                        dayRangeGenerator,
-                        sources,
-                        new SystemClock()
-                );
-        ExecutorService executor = Executors.newFixedThreadPool(
-                10,
-                new ThreadFactoryBuilder().setDaemon(true)
-                        .setNameFormat("schedule-bootstrap-%d")
-                        .build()
-        );
-        return new ExecutorServiceScheduledTask<UpdateProgress>(
-                executor,
-                supplier,
-                10,
-                1,
-                TimeUnit.MINUTES
-        );
-    }
-
-    @Bean
     ChannelIntervalScheduleBootstrapTaskFactory scheduleBootstrapTaskFactory() {
         return new ChannelIntervalScheduleBootstrapTaskFactory(persistence.legacyScheduleStore(),
                 persistence.scheduleStore(),
@@ -179,11 +128,6 @@ public class BootstrapModule {
                         persistence.contentStore()
                 )
         );
-    }
-
-    @PostConstruct
-    public void schedule() {
-        scheduler.scheduler().schedule(scheduleBootstrapTask(), RepetitionRules.NEVER);
     }
 
     @Bean
