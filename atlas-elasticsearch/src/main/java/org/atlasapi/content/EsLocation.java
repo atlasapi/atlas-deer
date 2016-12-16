83144ffbdf18031a622af9af88b8d718633242ab
Jamie Coleman <jamiec@metabroadcast.com>
2016-12-16 11:47:41 +0000

    remove unused import

--- atlas-elasticsearch/src/main/java/org/atlasapi/content/EsLocation.java
+++ atlas-elasticsearch/src/main/java/org/atlasapi/content/EsLocation.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.atlasapi.entity.Alias;
