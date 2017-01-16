f54fc8190e3b40b7cff872babb16f9e196493906
Jamie Coleman <jamiec@metabroadcast.com>
2017-01-16 18:03:50 +0000

    Change naming to avoid possible conflicts

--- atlas-api/src/main/java/org/atlasapi/system/SourcesModule.java
+++ atlas-api/src/main/java/org/atlasapi/system/SourcesModule.java
@@ -1,17 +1,14 @@
 package org.atlasapi.system;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SourcesModule {
 
-    private @Autowired SourcesController sourcesController;
-
     @Bean
-    public SourcesController sourcesController() {
-        return SourcesController.create();
+    public SystemSourcesController systemSourcesControllergit () {
+        return SystemSourcesController.create();
     }
 
 }
