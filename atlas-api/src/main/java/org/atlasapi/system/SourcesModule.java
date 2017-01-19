5324d34dfd168cf7a9cc0d87b9794084f55905be
jamiesprax <jamiec@metabroadcast.com>
2017-01-19 15:13:18 +0000

    use source writer with ID's (#503)

--- atlas-api/src/main/java/org/atlasapi/system/SourcesModule.java
+++ atlas-api/src/main/java/org/atlasapi/system/SourcesModule.java
@@ -1,14 +1,20 @@
 package org.atlasapi.system;
 
+import com.metabroadcast.common.ids.NumberToShortStringCodec;
+import com.metabroadcast.common.ids.SubstitutionTableNumberCodec;
+import org.atlasapi.application.sources.SourceIdCodec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SourcesModule {
 
+    private final NumberToShortStringCodec idCodec = SubstitutionTableNumberCodec.lowerCaseOnly();
+    private final SourceIdCodec sourceIdCodec = new SourceIdCodec(idCodec);
+
     @Bean
     public SystemSourcesController systemSourcesControllergit () {
-        return SystemSourcesController.create();
+        return SystemSourcesController.create(sourceIdCodec);
     }
 
 }
