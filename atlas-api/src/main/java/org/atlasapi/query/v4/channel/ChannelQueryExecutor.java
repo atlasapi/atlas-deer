cfe2cb4529570dd080f226f8f22802e05c9196fe
Jamie Coleman <jamiec@metabroadcast.com>
2017-08-02 19:18:45 +0100

    fix param name

--- atlas-api/src/main/java/org/atlasapi/query/v4/channel/ChannelQueryExecutor.java
+++ atlas-api/src/main/java/org/atlasapi/query/v4/channel/ChannelQueryExecutor.java
@@ -52,7 +52,6 @@ public class ChannelQueryExecutor implements QueryExecutor<ResolvedChannel> {
 
     private static final String TITLE = "title";
     private static final String TITLE_REVERSE = "title.reverse";
-    private static final String REFRESH_CACHE_PARAM = "refreshCache";
 
     private final ChannelResolver channelResolver;
     private final ChannelGroupResolver channelGroupResolver;
@@ -86,7 +85,7 @@ public class ChannelQueryExecutor implements QueryExecutor<ResolvedChannel> {
                 Futures.transform(
                         channelResolver.resolveIds(
                                 ImmutableSet.of(query.getOnlyId()),
-                                Boolean.parseBoolean(query.getContext().getRequest().getParameter(REFRESH_CACHE_PARAM))
+                                Boolean.parseBoolean(query.getContext().getRequest().getParameter(Attributes.REFRESH_CACHE_PARAM))
                         ),
                         (Function<Resolved<Channel>, QueryResult<ResolvedChannel>>) input -> {
                             if (input.getResources().isEmpty()) {
