d0eb71cfd188cc71276a779e8d96de905e2cec2e
Jamie Coleman <jamiec@metabroadcast.com>
2017-09-21 18:03:34 +0100

    the full content resolver still needs to resolve all the content

--- atlas-api/src/main/java/org/atlasapi/query/common/FullContentResolver.java
+++ atlas-api/src/main/java/org/atlasapi/query/common/FullContentResolver.java
@@ -1,21 +1,26 @@
 package org.atlasapi.query.common;
 
+import com.metabroadcast.common.stream.MoreCollectors;
 import org.atlasapi.content.Content;
 import org.atlasapi.content.ResolvedContent;
+import org.atlasapi.query.common.context.QueryContext;
 import org.atlasapi.topic.Topic;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class FullContentResolver {
 
     public QueryResult<ResolvedContent> resolve(QueryResult<Content> queryResult) {
         if (queryResult.isListResult()) {
             return QueryResult.listResult(
-                    resolveContent(queryResult.getResources()),
+                    resolveContent(queryResult.getResources(), queryResult.getContext()),
                     queryResult.getContext(),
                     queryResult.getTotalResults()
             );
         } else {
             return QueryResult.singleResult(
-                    resolveContent(queryResult.getOnlyResource()),
+                    resolveContent(queryResult.getOnlyResource(), queryResult.getContext()),
                     queryResult.getContext()
             );
         }
@@ -35,18 +40,20 @@ public class FullContentResolver {
             QueryResult.ListQueryResult<Content> listQuery
     ) {
         return QueryResult.ListQueryResult.listResult(
-                resolveContent(listQuery.getResources()),
+                resolveContent(listQuery.getResources(), listQuery.getContext()),
                 listQuery.getContext(),
                 listQuery.getTotalResults()
         );
     }
 
-    private ResolvedContent resolveContent(Content content) {
+    private ResolvedContent resolveContent(Content content, QueryContext context) {
         return null;
     }
 
-    private Iterable<ResolvedContent> resolveContent(Iterable<Content> contents) {
-        return null;
+    private Iterable<ResolvedContent> resolveContent(Iterable<Content> contents, QueryContext context) {
+        return StreamSupport.stream(contents.spliterator(), false)
+                .map(content -> resolveContent(content, context))
+                .collect(MoreCollectors.toImmutableList());
     }
 
 }
