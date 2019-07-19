8fabe771a74134a6665794923e4ab450503695c7
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2019-07-19 16:45:37 +0100

    Try adding ApplicationModule to AtlasProcessingModule

--- atlas-processing/src/main/java/org/atlasapi/AtlasProcessingModule.java
+++ atlas-processing/src/main/java/org/atlasapi/AtlasProcessingModule.java
@@ -1,14 +1,13 @@
 package org.atlasapi;
 
+import com.metabroadcast.common.webapp.properties.ContextConfigurer;
+import org.atlasapi.application.ApplicationModule;
 import org.atlasapi.messaging.KafkaMessagingModule;
 import org.atlasapi.messaging.WorkersModule;
 import org.atlasapi.system.ProcessingHealthModule;
 import org.atlasapi.system.ProcessingMetricsModule;
 import org.atlasapi.system.bootstrap.BootstrapModule;
 import org.atlasapi.system.debug.DebugModule;
-
-import com.metabroadcast.common.webapp.properties.ContextConfigurer;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,6 +20,7 @@ import org.springframework.context.annotation.Import;
         AtlasPersistenceModule.class,
         WorkersModule.class,
         BootstrapModule.class,
+        ApplicationModule.class,
         DebugModule.class
 })
 public class AtlasProcessingModule {
