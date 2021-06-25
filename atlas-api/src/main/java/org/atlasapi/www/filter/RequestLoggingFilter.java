9b16d7d4a69b860a5b2e409acc192f7259e59e54
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2021-06-25 11:11:06 +0100

    Match CorsFilter structure since new filter was not triggering

--- atlas-api/src/main/java/org/atlasapi/www/filter/RequestLoggingFilter.java
+++ atlas-api/src/main/java/org/atlasapi/www/filter/RequestLoggingFilter.java
@@ -4,35 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 
-@Component
-@WebFilter("/*")
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class RequestLoggingFilter implements Filter {
+public class RequestLoggingFilter extends OncePerRequestFilter {
     private static final Logger log = LoggerFactory.getLogger(RequestLoggingFilter.class);
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // do nothing
-    }
-
-    @Override
-    public void doFilter(
-            ServletRequest req,
-            ServletResponse resp,
+    protected void doFilterInternal(
+            HttpServletRequest req,
+            HttpServletResponse resp,
             FilterChain chain
     ) throws IOException, ServletException {
         Instant start = Instant.now();
@@ -41,18 +30,9 @@ public class RequestLoggingFilter implements Filter {
         } finally {
             Instant finish = Instant.now();
             long time = Duration.between(start, finish).toMillis();
-            if (req instanceof HttpServletRequest) {
-                String fullRequestURL = fullRequestURL((HttpServletRequest) req);
-                log.info("{}: {}ms", fullRequestURL, time);
-            } else {
-                log.warn("Encountered a request that could not be logged");
-            }
+            String fullRequestURL = fullRequestURL(req);
+            log.info("{} {}ms", fullRequestURL, time);
         }
-    }
-
-    @Override
-    public void destroy() {
-        // do nothing
     }
 
     private String fullRequestURL(HttpServletRequest request) {
